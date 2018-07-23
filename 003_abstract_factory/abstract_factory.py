59cf5a97df2ad058f56e095ed9badccc1a117b8a
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2018-07-23 13:46:26 -0500

    7 - Abstract Factory - added my TouchFactory realisation

--- 003_abstract_factory/abstract_factory.py
+++ 003_abstract_factory/abstract_factory.py
@@ -7,6 +7,10 @@ class Shape3DInterface:
     def build(self): pass
 
 
+class TouchInterface:
+    def touch(self): pass
+
+
 # === concrete shape classes ===
 class Circle(Shape2DInterface):
     def draw(self):
@@ -21,6 +25,22 @@ class Square(Shape2DInterface):
 class Sphere(Shape3DInterface):
     def build(self):
         print('Sphere.build')
+
+
+class Slide(TouchInterface, Shape3DInterface):
+    def touch(self):
+        print('touching the Slide')
+
+    def build(self):
+        print('building the Slide...')
+
+
+class Panel(TouchInterface, Shape2DInterface):
+    def touch(self):
+        print('touching the Panel')
+
+    def draw(self):
+        print('Drawing the Panel')
 
 
 class Cube(Shape3DInterface):
@@ -55,11 +75,20 @@ class Shape3DFactory(ShapeFactoryInterface):
         assert 0, 'Bad 3D shape creation: shape factory not defined for ' + sides + ' sides'
 
 
+class ShapeTouchFactory(ShapeFactoryInterface):
+    @staticmethod
+    def getShape(sides):
+        if sides == 1:
+            return Slide()
+        if sides == 2:
+            return Panel()
+        assert 0, 'Bad touch request: shape factory is not defined for ' + sides + ' sides'
+
+
 shape2DFactory = Shape2DFactory()
 circle = shape2DFactory.getShape(1)
 print(circle)
 circle.draw()
-
 
 shape3DFactory = Shape3DFactory()
 sphere = shape3DFactory.getShape(1)
@@ -67,6 +96,11 @@ print(sphere)
 sphere.build()
 
 
+touchFactory = ShapeTouchFactory()
+slide = ShapeTouchFactory.getShape(1)
+slide.touch()
+slide.build()
 
-
-
+panel = ShapeTouchFactory.getShape(2)
+panel.touch()
+panel.draw()
