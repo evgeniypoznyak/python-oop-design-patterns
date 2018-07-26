e9922f9b8d40a82676b5d76a7a6fdb298f30366a
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2018-07-26 00:26:42 -0500

    15 - Adapter

---
+++ 010_adapter/adapter.py
@@ -0,0 +1,71 @@
+# Adaptee (source) interface
+
+class EuropeanSocketInterface:
+    def voltage(self): pass
+
+    def live(self): pass
+
+    def neutral(self): pass
+
+    def earth(self): pass
+
+
+# Target interface
+class USASocketInterface:
+    def voltage(self): pass
+
+    def live(self): pass
+
+    def neutral(self): pass
+
+
+class EuropeanSocket(EuropeanSocketInterface):
+    def voltage(self):
+        return 230
+
+    def live(self):
+        return 1
+
+    def neutral(self):
+        return -1
+
+
+# Client
+class AmericanKettle:
+    __power = None
+
+    def __init__(self, power):
+        self.__power = power
+
+    def boil(self):
+        if self.__power.voltage() > 110:
+            print('Fire!!!')
+        else:
+            if self.__power.live() == 1 and self.__power.neutral() == -1:
+                print('Works!')
+            else:
+                print('No power')
+
+
+class Adapter(USASocketInterface):
+    __socket = None
+
+    def __init__(self, socket):
+        self.__socket = socket
+
+    def voltage(self):
+        return 110
+
+    def live(self):
+        return self.__socket.live()
+
+    def neutral(self):
+        return self.__socket.neutral()
+
+
+outlet = AmericanKettle(EuropeanSocket())
+outlet.boil()
+
+adapter = Adapter(EuropeanSocket())
+outlet = AmericanKettle(adapter)
+outlet.boil()
